cf8596abe744671ab7e0f7e01d0a3d3c54201188
Adam J Gray <agray@salesforce.com>
2016-08-25 10:59:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplerails::Application.config.secret_token = '7013c9a5de7369fb5cf684d4898781c8f8d6533294734527513981783cb659257ea47c0f8f0ac1af715db7dba45d0ad152e77c502ad736f87c320db2d2afcb52'
